1c23e2925947188808169ea3198e6e6534ba9c92
David Reed <david@ktema.org>
2018-12-23 22:14:39 -0500

    Add unit test for transforms

---
+++ amaxa/test_transforms.py
@@ -0,0 +1,8 @@
+import unittest
+from . import transforms
+
+class test_transforms(unittest.TestCase):
+    def test_transforms(self):
+        self.assertEqual('test', transforms.strip('  test  '))
+        self.assertEqual('test', transforms.lowercase('TEst'))
+        self.assertEqual('TEST', transforms.uppercase('tesT'))
