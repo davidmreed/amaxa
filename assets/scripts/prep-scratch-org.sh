8aeb160285a3cdf8bb8c405496b3b87f5cbe277d
David Reed <david@ktema.org>
2019-01-15 21:35:27 -0500

    Clean up CI scripts

--- assets/scripts/prep-scratch-org.sh
+++ assets/scripts/prep-scratch-org.sh
@@ -1,3 +1,3 @@
 sfdx force:org:create -v DevHub -s -f assets/project-scratch-def.json -a scratch
 sfdx force:data:tree:import -p assets/test_data_sfdx/Account-Contact-plan.json -u scratch
-source get-auth-params.sh
+source assets/scripts/get-auth-params.sh
