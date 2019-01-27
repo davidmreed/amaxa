c0a460359caab0ad9b1049be1c0481eea565355f
David Reed <david@ktema.org>
2019-01-27 15:03:25 -0500

    Correct setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='amaxa',
-    version='0.1.0',
+    version='0.9.0',
     description='Load and extract data from multiple Salesforce objects in a single operation, preserving links and network structure.',
     author='David Reed',
     author_email='david@ktema.org',
-    license='MIT License',
+    license='GNU GPLv3',
     packages=['amaxa'],
     python_requires='>=3.6',
-    requires=['pyyaml', 'simple_salesforce', 'salesforce-bulk', 'cerberus'],
+    requires=['pyyaml', 'simple_salesforce', 'salesforce_bulk', 'cerberus'],
     tests_require=['pytest', 'pytest-cov', 'codecov', 'wheel'],
     entry_points={
         'console_scripts': [
