436af2da17e54f5f842cf9225d26687e54ab3072
David Reed <david@ktema.org>
2019-01-28 17:42:07 -0500

    Correct setup.py [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='amaxa',
-    version='0.9.0',
+    version='0.9.1',
     description='Load and extract data from multiple Salesforce objects in a single operation, preserving links and network structure.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,7 +15,7 @@ setup(
     license='GNU GPLv3',
     packages=['amaxa'],
     python_requires='>=3.6',
-    requires=['pyyaml', 'simple_salesforce', 'salesforce_bulk', 'cerberus', 'requests', 'pyjwt'],
+    install_requires=['pyyaml', 'simple_salesforce', 'salesforce_bulk', 'cerberus', 'requests', 'pyjwt'],
     entry_points={
         'console_scripts': [
             'amaxa = amaxa.__main__:main'
