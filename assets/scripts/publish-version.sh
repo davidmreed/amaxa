2bf0c817b2a63e3a2ba880bc9668c6545f4ee0d6
David Reed <david@ktema.org>
2020-03-23 02:43:42 +0000

    Expand GitHub Actions usage; remove publishing from GitLab; restore Python 3.6

--- assets/scripts/publish-version.sh
+++ assets/scripts/publish-version.sh
@@ -2,4 +2,8 @@ export BIN_PATH=$(curl --request POST --header "Private-Token: $API_TOKEN" --for
 export WHEEL_PATH=$(curl --request POST --header "Private-Token: $API_TOKEN" --form "file=@dist/amaxa-$CI_COMMIT_TAG-py3-none-any.whl" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/uploads" | jq -r .url)
 export SDIST_PATH=$(curl --request POST --header "Private-Token: $API_TOKEN" --form "file=@dist/amaxa-$CI_COMMIT_TAG.tar.gz" "$CI_API_V4_URL/projects/$CI_PROJECT_ID/uploads" | jq -r .url)
 export DATA="{ \"name\": \"$CI_COMMIT_TAG\", \"tag_name\": \"$CI_COMMIT_TAG\", \"assets\": { \"links\": [{ \"name\": \"Binary\", \"url\": \"$CI_PROJECT_URL$BIN_PATH\" }, { \"name\": \"Wheel\", \"url\": \"$CI_PROJECT_URL$WHEEL_PATH\" }, { \"name\": \"Sdist\", \"url\": \"$CI_PROJECT_URL$SDIST_PATH\" }] } }"
-curl --header "Content-Type: application/json"  --header "Private-Token: $API_TOKEN" --data "$DATA" --request POST "$CI_API_V4_URL/projects/$CI_PROJECT_ID/releases"
+
+curl --header "Content-Type: application/json" \
+--header "Private-Token: $API_TOKEN" \
+--data "$DATA" \
+--request POST "$CI_API_V4_URL/projects/$CI_PROJECT_ID/releases"
